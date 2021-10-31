371305c0e633d3f19d1677e843a0f321a270029d
Daniil Verkhutin <daniil.verkhutin.it.2021@lpnu.ua>
2021-10-31 21:17:36 +0300

    commit2

--- Lab5.5/lab5.5.cpp
+++ Lab5.5/lab5.5.cpp
@@ -2,25 +2,32 @@
 
 using namespace std;
 
-double C(int k, int n) {
-	if (k == n || k == 0)
-		return 1;
-	else
-		if (n > 0) {
-			return C(k - 1, n - 1) + C(k, n - 1);
-		}
+double C(int k, int n, int level, int &depth) {
+	
+		if (k == n || k == 0)
+			return 1;
 		else
-			return 0;
+			if (n > 0) {
+				if (level > depth) {
+					depth = level;
+					return C(k - 1, n - 1, level + 1, depth) + C(k, n - 1, level + 1, depth);
+				}
+			}
+			else
+				return 0;
+
 }
 
 int main() {
 	int k, n;
+	int depth = 0;
 
 	cout << "k = "; cin >> k;
 	cout << "n = "; cin >> n;
 	cout << endl;
 
-	cout << "Depth = " << n << endl;
-	cout << "C = " << C(k, n);
+	
+	cout << "C = " << C(k, n, 1, depth) << endl;
+	cout << "Depth = " << depth << endl;
 	
 }
